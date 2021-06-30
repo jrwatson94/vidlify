7d6634a094b141cdd1dae3efc998ee23fb06c465
jrwatson94 <j.watsonreid@gmail.com>
2021-06-29 22:00:24 -0400

    S3 video storage functional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
 import './App.css';
-import Nav from './Nav/Nav'
-import Menu from './Menu/Menu'
-import VidDisplay from './VidDisplay/VidDisplay'
+import React, {useState, useEffect} from 'react';
+import Nav from './Nav/Nav';
+import Menu from './Menu/Menu';
+import VidDisplay from './VidDisplay/VidDisplay';
+import Amplify, { Storage } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
 
 
 function App() {
+  const [videoUrl, setVideoUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [fileName, setFileName]=useState(null);
+  const [videos, setVideos] = useState([]);
+
+  useEffect( async () => {
+    const vids = await Storage.list('')
+    setVideos(vids);
+    console.log(vids)
+  }, [fileName])
+  
+  
+  const displayVidLinks = () =>{
+
+    const clickHandler = async (key) => {
+      const url = await Storage.get(key);
+      setVideoUrl(url);
+      setFileName(key);
+    }
+    
+    return videos.map(vid => {
+      return <a><li onClick={ () => clickHandler(vid.key)}>{vid.key}</li></a>
+    })
+  }
+
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+    try {
+      setLoading(true);
+      // Upload the file to s3 with private access level. 
+      await Storage.put(file.name, file, {
+        level: 'public',
+        contentType: 'video'
+      });
+      // Retrieve the uploaded file to display
+      const url = await Storage.get(`${file.name}`)
+      console.log(url)
+      setFileName(file.name)
+      setVideoUrl(url);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="App">
       <h1>Welcome to Vidlify!</h1>
+      <h1>My Videos</h1>
+      <div id="vid-container">
+        {displayVidLinks()}
+      </div>
+      <h1> Upload a Video </h1>
+      {loading ? <h3>Uploading...</h3> : <input
+        type="file" accept='video/*'
+        onChange={(evt) => handleChange(evt)}
+      />}
+      <div>
+        {videoUrl ? <video controls type="video/mp4" style={{ width: "30rem" }} src={videoUrl} /> : <span />}
+      </div>
       <Nav />
       <Menu />
       <VidDisplay url={"most recent url"}/>
@@ -15,4 +78,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Nav.css';
+
 function Nav() {
     return (
       <div className="App">
